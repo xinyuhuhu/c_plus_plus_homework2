202c98fed7f15405829112946681cd38838829be
xinyuhuhu <77626799+xinyuhuhu@users.noreply.github.com>
2021-01-19 15:51:50 +0800

    Update 2-7-2.cpp

--- 2-7-2.cpp
+++ 2-7-2.cpp
@@ -1,22 +1,31 @@
+// function calculates fibonacci number
 #include<iostream>
 #include<cmath>
 using namespace std;
 
+int times2 = 1;
+
 int power(int a,int n)
-{ static int k=1;
+{ 
+  ++times2;
   if(n==0)
     return 1;
+
   else if(n%2==0)
   {
-    cout<<"It's the "<<k<<"-th recursive call"<<endl;
-    k = k+1;
-    return power(a,n/2)*power(a, n/2);
+    int half{};
+    long long int temp{};
+    half = n/2;
+    temp = power(a, half);
+    return temp*temp;
   }
   else
   {
-    cout<<"It's the "<<k<<"-th recursive call"<<endl;
-    k = k+1;
-    return a * power(a,n/2) * power(a, n/2);
+    int half{};
+    long long int temp{};
+    half = n/2;
+    temp = power(a,half);
+    return a*temp*temp;
   }
 }
 
@@ -24,11 +33,16 @@ int main()
 {
   int a;
   int n;
+
   cout << "please input a number that you want to power: "<<endl;
   cin >> a;
+
   cout << "please input the power of this number"<<endl;
   cin >> n;
 
   cout << "the power of this number is " << endl << power(a,n) << endl;
+  cout << "the number of recursive times is: "<< times2 << endl;
+
   return 0;
 }
+
