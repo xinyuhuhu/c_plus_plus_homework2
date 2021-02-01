1eaf4c897b7f5fd8342034e46a98483cfec7a8ff
xinyuhuhu <77626799+xinyuhuhu@users.noreply.github.com>
2021-02-01 23:29:40 +0800

    Update part1-exercise2-c++style.cpp

--- exercise5/part1-exercise2-c++style.cpp
+++ exercise5/part1-exercise2-c++style.cpp
@@ -1,29 +1,25 @@
-//  exercise5-4
-//
-//  Created by Xinyu HU on 2021/2/1.
-//
+// exercise 5-2
 
 #include <iostream>
 #include <string>
 using namespace std;
 
 //judge whether a string contains only digits or not
-int repeat_times(string str)
+int occur_times(string str)
 {
     int count = 1;
-    
-    for (int i=0; i<str.size(); ++i)
+    for (int i=0; i<str.size(); i++)
     {
-        for (int j=0; j<str.size(); ++j)
+        for (int j=0; j<str.size();j++)
         {
             if ((str[i] == str[j]) && (str[i+1] == str[j+1]) && (i!=j))
             {
                 ++count;
             }
         }
-           
     }
-    return count;
+    //for every repeat has been counted 2 times
+    return (count+1)/2;
 }
 
 
@@ -33,7 +29,8 @@ int main()
     cout << "Please input a string you want: ";
     cin >> str;
 
-    cout << "The number of repeat time is: " << repeat_times(str) << endl;
+    cout << "The number of any a pair of characters occurs is: " << occur_times(str) << endl;
   
     return 0;
 }
+
